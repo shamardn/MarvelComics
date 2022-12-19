781b5ee9ab0fe1089c8bdb2f8e9cfb85471d24f7
SHAMARDN <shamardn@gmail.com>
2022-12-19 11:31:47 +0200

    improve MarvelDetails.kt composable arrangement

--- app/src/main/java/com/shamardn/android/marvelcomics/ui/composable/MarvelDetails.kt
+++ app/src/main/java/com/shamardn/android/marvelcomics/ui/composable/MarvelDetails.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -16,85 +17,68 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
 import com.shamardn.android.marvelcomics.R
-import com.shamardn.android.marvelcomics.ui.screen.characters.uistate.CharactersDetailsUiState
+import com.shamardn.android.marvelcomics.ui.screen.characterDetails.uistate.CharacterDetailsUiState
 
 @Composable
 fun MarvelDetails(
-    state: CharactersDetailsUiState,
+    state: CharacterDetailsUiState,
     onBackClick: () -> Unit,
 ) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(color = Color.White),
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .background(color = Color.White),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Top
-    ) {
+        verticalArrangement = Arrangement.Top) {
 
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(300.dp)
-        ) {
+        Box(modifier = Modifier
+            .fillMaxWidth()
+            .height(300.dp)) {
             val img = "${state.thumbnail.path}.${state.thumbnail.extension}"
             Image(
                 painter = rememberAsyncImagePainter(model = img),
                 contentDescription = "Description",
-                modifier = Modifier
-                    .fillMaxSize(),
+                modifier = Modifier.fillMaxSize(),
                 contentScale = ContentScale.FillBounds,
-                )
+            )
 
             BackIcon(
                 modifier = Modifier.align(Alignment.TopStart), onBackClick,
             )
         }
-        Row(
+
+        Text(
+            text = state.name,
+            color = MaterialTheme.colors.onBackground, style = MaterialTheme.typography.h5,
+            modifier = Modifier
+                .padding(vertical = 8.dp, horizontal = 16.dp),
+        )
+
+        Text(
+            text = state.modifiedDate.toString(),
+            color = MaterialTheme.colors.onBackground, style = MaterialTheme.typography.caption,
+            modifier = Modifier
+                .padding(vertical = 8.dp, horizontal = 16.dp),
+        )
+
+        Text(
+            text = stringResource(R.string.more),
+            color = MaterialTheme.colors.onBackground, style = MaterialTheme.typography.caption,
             modifier = Modifier
                 .padding(vertical = 8.dp, horizontal = 16.dp)
-                .fillMaxWidth(),
-            verticalAlignment = Alignment.Top,
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-                Text(
-                    text = state.name,
-                    color = MaterialTheme.colors.onBackground, style = MaterialTheme.typography.h5,
-                    modifier = Modifier,
-                )
-
-            Text(
-                text = state.modifiedDate.toString(),
-                color = MaterialTheme.colors.onBackground, style = MaterialTheme.typography.caption,
-                modifier = Modifier,
-            )
-        }
-
-        Row(
+                .align(Alignment.End)
+                .clickable { onBackClick() },
+        )
+        Text(
+            text = state.description,
+            color = MaterialTheme.colors.onBackground,
+            style = MaterialTheme.typography.subtitle2,
             modifier = Modifier
-                .padding(vertical = 8.dp, horizontal = 16.dp)
-                .fillMaxWidth(),
-            verticalAlignment = Alignment.Top,
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-                Text(
-                    text =  state.description,
-                    color = MaterialTheme.colors.onBackground,
-                    style = MaterialTheme.typography.subtitle2,
-                    modifier = Modifier,
-
-                    )
-
-            Text(
-                text = stringResource(R.string.more),
-                color = MaterialTheme.colors.onBackground, style = MaterialTheme.typography.caption,
-                modifier = Modifier
-                    .clickable { onBackClick() },
+                .padding(vertical = 8.dp, horizontal = 16.dp),
             )
-        }
 
         LazyColumn(
             verticalArrangement = Arrangement.spacedBy(16.dp),
-            modifier = Modifier,
+            modifier = Modifier.background(color = Color.Red),
         ) {
             item {
                 Text(
@@ -102,32 +86,18 @@ fun MarvelDetails(
                     color = MaterialTheme.colors.onBackground,
                     style = MaterialTheme.typography.body1,
                     modifier = Modifier
+                        .background(color = Color.Yellow)
                         .align(Alignment.Start)
                         .padding(horizontal = 16.dp),
                 )
             }
             item {
-//                LazyRow(
-//                    horizontalArrangement = Arrangement.spacedBy(16.dp),
-//                    contentPadding = PaddingValues(16.dp),
-//                ) {
-//                    items(items = state.marvelCharacters,
-//                        key = { currentCharacter ->
-//                            currentCharacter.name
-//                        }
-//                        ){
-//                        ItemComic(
-//                            state = it,
-//                            onClick = { onClickCharacter() },
-//                            modifier = Modifier
-//                                .animateItemPlacement()
-//                                .size(150.dp, 100.dp)
-//                        )
-//                    }
-//
-//                }
+                LazyRow(horizontalArrangement = Arrangement.spacedBy(16.dp),
+                    contentPadding = PaddingValues(16.dp)) {
+
+                }
             }
         }
-
     }
+
 }
