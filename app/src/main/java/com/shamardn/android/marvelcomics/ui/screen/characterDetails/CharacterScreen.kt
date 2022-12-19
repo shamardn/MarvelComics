2955462ee62906c2ba12d95eb5db5d179e59d126
SHAMARDN <shamardn@gmail.com>
2022-12-19 11:08:58 +0200

    improve navigation in CharacterScreen.kt and move navigation code to CharacterViewModel.kt

--- app/src/main/java/com/shamardn/android/marvelcomics/ui/screen/characterDetails/CharacterScreen.kt
+++ app/src/main/java/com/shamardn/android/marvelcomics/ui/screen/characterDetails/CharacterScreen.kt
@@ -1,4 +1,4 @@
-package com.shamardn.android.marvelcomics.ui.screen.characters
+package com.shamardn.android.marvelcomics.ui.screen.characterDetails
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -7,7 +7,7 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.shamardn.android.marvelcomics.Screen
 import com.shamardn.android.marvelcomics.ui.composable.MarvelDetails
-import com.shamardn.android.marvelcomics.ui.screen.characters.uistate.CharactersDetailsUiState
+import com.shamardn.android.marvelcomics.ui.screen.characterDetails.uistate.CharacterDetailsUiState
 
 @Composable
 fun CharacterScreen(
@@ -16,26 +16,21 @@ fun CharacterScreen(
 ) {
     val state by viewModel.state.collectAsState()
 
-    val currentCharacter =
-        navController.previousBackStackEntry?.savedStateHandle?.get<CharactersDetailsUiState>(Screen.Character.route)
-
     CharacterContent(
         state = state,
         onBackClick = { navController.popBackStack(Screen.Home.route, false) }
     )
 
-    if (currentCharacter != null) {
-        viewModel.updateCurrentCharacter(currentCharacter)
-    }
 }
 
 @Composable
 fun CharacterContent(
-    state: CharactersDetailsUiState,
+    state: CharacterDetailsUiState,
     onBackClick: () -> Unit,
 ) {
     MarvelDetails(
         state = state,
-        onBackClick,
+        onBackClick = onBackClick,
+        onClickComic = {},
     )
 }
