cc9a49dc9dcffad4466daca97fc32625cfd26d88
SHAMARDN <shamardn@gmail.com>
2022-12-19 10:59:53 +0200

    remove Parcelable annotations from CharactersDetailsUiState

--- app/src/main/java/com/shamardn/android/marvelcomics/ui/screen/characterDetails/uistate/CharacterDetailsUiState.kt
+++ app/src/main/java/com/shamardn/android/marvelcomics/ui/screen/characterDetails/uistate/CharacterDetailsUiState.kt
@@ -1,16 +1,13 @@
-package com.shamardn.android.marvelcomics.ui.screen.characters.uistate
+package com.shamardn.android.marvelcomics.ui.screen.characterDetails.uistate
 
-import android.os.Parcelable
 import com.shamardn.android.marvelcomics.domain.model.Thumbnail
-import kotlinx.parcelize.Parcelize
 import java.util.*
 
-@Parcelize
-data class CharactersDetailsUiState(
+data class CharacterDetailsUiState(
     val id: Int = 0,
     val name: String = "",
     val description: String = "",
     val modifiedDate: Date = Date(),
     val thumbnail: Thumbnail = Thumbnail("http://i.annihil.us/u/prod/marvel/i/mg/3/40/4bb4680432f73","jpg"),
     val resourceURI: String = "",
-): Parcelable
+)
