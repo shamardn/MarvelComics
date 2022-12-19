bf13273ab515f7c86913b5eb53a2257628d49c56
SHAMARDN <shamardn@gmail.com>
2022-12-19 11:38:44 +0200

    add endpoint of getCharacterId to MarvelService.kt

--- app/src/main/java/com/shamardn/android/marvelcomics/data/remote/service/MarvelService.kt
+++ app/src/main/java/com/shamardn/android/marvelcomics/data/remote/service/MarvelService.kt
@@ -3,10 +3,14 @@ package com.shamardn.android.marvelcomics.data.remote.service
 import com.shamardn.android.marvelcomics.data.remote.response.base.BaseResponse
 import com.shamardn.android.marvelcomics.data.remote.response.dto.MarvelCharacterDTO
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface MarvelService {
     @GET("characters")
-    suspend fun getMarvelCharacters(): BaseResponse<MarvelCharacterDTO>?
+    suspend fun getMarvelCharacters(): BaseResponse<MarvelCharacterDTO>
 
-
+    @GET("characters/{characterId}")
+    suspend fun getCharacterId(
+        @Path("characterId") characterId: Int,
+    ): BaseResponse<MarvelCharacterDTO>
 }
