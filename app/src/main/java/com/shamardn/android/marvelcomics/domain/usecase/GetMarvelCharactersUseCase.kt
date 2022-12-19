9664a0075c3c6c9cb775b854a160ef6d31881cf4
SHAMARDN <shamardn@gmail.com>
2022-12-19 11:11:27 +0200

    improve GetMarvelCharactersUseCase.kt implementation using CharacterMapper.map

--- app/src/main/java/com/shamardn/android/marvelcomics/domain/usecase/GetMarvelCharactersUseCase.kt
+++ app/src/main/java/com/shamardn/android/marvelcomics/domain/usecase/GetMarvelCharactersUseCase.kt
@@ -1,14 +1,15 @@
 package com.shamardn.android.marvelcomics.domain.usecase
 
 import com.shamardn.android.marvelcomics.data.repository.MarvelRepository
-import com.shamardn.android.marvelcomics.domain.mapper.toMarvelCharacter
+import com.shamardn.android.marvelcomics.domain.mapper.CharacterMapper
 import com.shamardn.android.marvelcomics.domain.model.MarvelCharacter
 import javax.inject.Inject
 
 class GetMarvelCharactersUseCase @Inject constructor(
     private val marvelRepository: MarvelRepository,
+    private val characterMapper: CharacterMapper,
 ){
-     suspend operator fun invoke(): List<MarvelCharacter>? {
-         return marvelRepository.getMarvelCharacters()?.data?.results?.map { it.toMarvelCharacter( it.thumbnail?.path!!, it.thumbnail.extension!!) }
+     suspend operator fun invoke(): List<MarvelCharacter> {
+         return characterMapper.map(marvelRepository.getMarvelCharacters())
     }
 }
