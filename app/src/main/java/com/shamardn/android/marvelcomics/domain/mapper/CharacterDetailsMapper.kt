775d09ce5540f5bc779a7bd24ea0dc3e51d3eda3
SHAMARDN <shamardn@gmail.com>
2022-12-19 11:42:51 +0200

    add CharacterDetailsMapper.kt and ThumbnailMapper.kt in Domain layer

---
+++ app/src/main/java/com/shamardn/android/marvelcomics/domain/mapper/CharacterDetailsMapper.kt
@@ -0,0 +1,31 @@
+package com.shamardn.android.marvelcomics.domain.mapper
+
+import com.shamardn.android.marvelcomics.data.remote.response.base.BaseResponse
+import com.shamardn.android.marvelcomics.data.remote.response.common.ThumbnailDTO
+import com.shamardn.android.marvelcomics.data.remote.response.dto.MarvelCharacterDTO
+import com.shamardn.android.marvelcomics.domain.model.MarvelCharacter
+import com.shamardn.android.marvelcomics.utils.convertStringToDate
+import javax.inject.Inject
+
+class CharacterDetailsMapper @Inject constructor(
+    private val thumbnailMapper: ThumbnailMapper,
+) : Mapper<BaseResponse<MarvelCharacterDTO>, MarvelCharacter>() {
+    override fun map(input: BaseResponse<MarvelCharacterDTO>): MarvelCharacter {
+        var thumbnailNotNull =
+            ThumbnailDTO(
+                path = "http://i.annihil.us/u/prod/marvel/i/mg/3/40/4bb4680432f73",
+                extension = "jpg",
+            )
+        if (input.data?.results?.first()?.thumbnail != null) {
+            thumbnailNotNull = input.data.results.first().thumbnail!!
+        }
+        return MarvelCharacter(
+            id = input.data?.results?.first()?.id ?: 0,
+            name = input.data?.results?.first()?.name ?: "",
+            description = input.data?.results?.first()?.description ?: "",
+            modifiedDate = convertStringToDate(input.data?.results?.first()?.modified),
+            thumbnail = thumbnailMapper.map(thumbnailNotNull),
+            resourceURI = input.data?.results?.first()?.resourceURI ?: ""
+        )
+    }
+}
