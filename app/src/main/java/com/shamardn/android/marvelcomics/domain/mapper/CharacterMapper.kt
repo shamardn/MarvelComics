4419a76e139cffdd746278778871580a796cd0e3
SHAMARDN <shamardn@gmail.com>
2022-12-19 11:04:02 +0200

    add abstract class Mapper and improve CharacterMapper.kt

--- app/src/main/java/com/shamardn/android/marvelcomics/domain/mapper/CharacterMapper.kt
+++ app/src/main/java/com/shamardn/android/marvelcomics/domain/mapper/CharacterMapper.kt
@@ -1,33 +1,28 @@
 package com.shamardn.android.marvelcomics.domain.mapper
 
-import com.shamardn.android.marvelcomics.data.remote.response.common.ThumbnailDTO
+import com.shamardn.android.marvelcomics.data.remote.response.base.BaseResponse
 import com.shamardn.android.marvelcomics.data.remote.response.dto.MarvelCharacterDTO
 import com.shamardn.android.marvelcomics.domain.model.MarvelCharacter
-import com.shamardn.android.marvelcomics.domain.model.Thumbnail
-import java.text.SimpleDateFormat
-import java.util.*
+import com.shamardn.android.marvelcomics.utils.convertStringToDate
+import javax.inject.Inject
 
-fun MarvelCharacterDTO.toMarvelCharacter(path: String, extension: String): MarvelCharacter {
-        return MarvelCharacter(
-            id = id ?: 0,
-            name = name ?: "",
-            description = description ?: "",
-            modifiedDate = modified?.convertStringToDate() ?: Date(),
-           thumbnail = thumbnail?.toThumbnail(path, extension) ?: Thumbnail("http://i.annihil.us/u/prod/marvel/i/mg/3/40/4bb4680432f73","jpg"),
-            resourceURI = resourceURI ?: ""
-        )
-    }
-
-fun ThumbnailDTO.toThumbnail(path: String, extension: String): Thumbnail{
-    return Thumbnail(path, extension)
-}
-
-fun String.convertStringToDate(): Date {
-    return if (this.isEmpty()) {
-        Date()
-    } else {
-        val formatter = SimpleDateFormat("yyyy-MM-dd", Locale("en"))
-        formatter.parse(this) ?: Date()
+class CharacterMapper @Inject constructor(
+    private val thumbnailMapper: ThumbnailMapper,
+) : Mapper<BaseResponse<MarvelCharacterDTO>, List<MarvelCharacter>>() {
+    override fun map(input: BaseResponse<MarvelCharacterDTO>): List<MarvelCharacter> {
+        var result = listOf<MarvelCharacterDTO>()
+        if (input.data?.results != null ){
+            result = input.data.results
+        }
+        return result.map {
+            MarvelCharacter(
+                id = it.id ?: 0,
+                name = it.name ?: "",
+                description = it.description ?: "",
+                modifiedDate = convertStringToDate(it.modified),
+                thumbnail = thumbnailMapper.map(it.thumbnail!!),
+                resourceURI = it.resourceURI ?: "",
+            )
+        }
     }
 }
-
