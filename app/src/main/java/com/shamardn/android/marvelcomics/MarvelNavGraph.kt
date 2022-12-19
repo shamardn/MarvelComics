fa3d03e6f62ce5cc48ce5307b71bf9ff6b14a7d8
SHAMARDN <shamardn@gmail.com>
2022-12-19 11:36:57 +0200

    change the way receiving args in MarvelNavGraph.kt and receive Int instead of parcelable object

--- app/src/main/java/com/shamardn/android/marvelcomics/MarvelNavGraph.kt
+++ app/src/main/java/com/shamardn/android/marvelcomics/MarvelNavGraph.kt
@@ -1,24 +1,22 @@
 package com.shamardn.android.marvelcomics
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.shamardn.android.marvelcomics.ui.screen.characters.CharacterScreen
-import com.shamardn.android.marvelcomics.ui.screen.characters.uistate.CharactersDetailsUiState
+import androidx.navigation.navArgument
+import com.shamardn.android.marvelcomics.ui.screen.characterDetails.CharacterScreen
 import com.shamardn.android.marvelcomics.ui.screen.home.HomeScreen
 
 @Composable
 fun MarvelNavGraph(navController: NavHostController) {
-    NavHost(navController = navController, startDestination = Screen.Home.route){
-    composable(Screen.Home.route){ HomeScreen(navController) }
-    composable(Screen.Character.route){
-        LaunchedEffect(key1 = it){
-            navController.previousBackStackEntry?.savedStateHandle?.get<CharactersDetailsUiState>(Screen.Character.route)
-        }
-         CharacterScreen(navController)
-    }
-
+    NavHost(navController = navController, startDestination = Screen.Home.route) {
+        composable(Screen.Home.route) { HomeScreen(navController) }
+        composable("${Screen.Character.route}/{id}",
+            arguments = listOf(
+                navArgument(name = "id") { NavType.IntType },
+            )
+        ) { CharacterScreen(navController) }
     }
 }
