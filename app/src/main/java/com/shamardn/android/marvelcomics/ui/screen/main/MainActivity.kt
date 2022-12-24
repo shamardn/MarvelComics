8e00c38f196fee294ca4a44f0724615e1adda625
SHAMARDN <shamardn@gmail.com>
2022-12-24 17:24:07 +0200

    add splashScreen

--- app/src/main/java/com/shamardn/android/marvelcomics/ui/screen/main/MainActivity.kt
+++ app/src/main/java/com/shamardn/android/marvelcomics/ui/screen/main/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @ExperimentalMaterialApi
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContent {
             MarvelComicsApp()
